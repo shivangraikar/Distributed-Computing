149e9906c41fd80c99c8aa25b81f4bf0292240b0
shivangraikar <51766272+shivangraikar@users.noreply.github.com>
2021-05-26 14:01:58 +0530

    name resolution protocol

--- Dns.java
+++ Dns.java
@@ -1,14 +1,12 @@
 import java.util.*; import java.lang.String;
 
 class dns{
-
-
+ 
 String subdns (String s)
 {
 
 if (s.contains(".") == true)
 {
-
 
 System.out.print("Now in Server: \t"); int dotloc = s.indexOf(".");
 StringBuilder sb1 = new StringBuilder(s.substring(0,dotloc)); sb1.reverse();
@@ -35,7 +33,6 @@ String subdns2 (String s)
 if (s.contains(".") == true)
 {
 
-
 System.out.print("Back to Server: "); int dotloc = s.indexOf(".");
 System.out.println(s.substring(0,dotloc)); dotloc++;
 s = s.substring(dotloc); return subdns2(s);
@@ -50,14 +47,12 @@ System.out.print("Back to Server: "); return(s);
 
 
 
-
 public static void main(String[] args)
 {
 
 Scanner sc = new Scanner(System.in); System.out.println("Enter a web address"); String ad= sc.nextLine();
  
 String[] arrOfStr= ad.split("[.]");
-
 
 System.out.println("\n------Iterative method	");
 System.out.println();
@@ -68,21 +63,9 @@ for (int i = arrOfStr.length-2; i >= 0; i--)
 System.out.print("Client's name resolver --> Name Server node :\t"); System.out.println(arrOfStr[i]);
 }
 
-
-
-
-
-
 System.out.println("\nURL found");
 
-
-
-
-
-
-
 System.out.println("\n\n------Recursive method	\n");
-
 
 dns d = new dns();
 StringBuilder sb = new StringBuilder(ad); sb.reverse(); System.out.println(d.subdns(sb.toString())); System.out.println();
@@ -95,8 +78,5 @@ System.out.println(d.subdns2(ad2));
 
 System.out.println("\nURL found");
 }
-
-
-
 
 }
