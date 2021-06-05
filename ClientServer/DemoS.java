55c43fff65492f2b5fe56cfea11004287f19ed7c
shivangraikar <51766272+shivangraikar@users.noreply.github.com>
2021-06-05 15:31:55 +0530

    Server side program

--- ClientServer/DemoS.java
+++ ClientServer/DemoS.java
@@ -1,1 +1,25 @@
+import java.io.*; import java.net.*;
 
+public class DemoS
+{
+
+public static void main(String[] args)
+{
+
+try
+{
+ServerSocket ss = new ServerSocket(2000); Socket s = ss.accept();
+DataInputStream dis = new
+DataInputStream(s.getInputStream());
+System.out.println("Server is waiting for the client..."); System.out.println("Message sent by the client is = " + (String)dis.readUTF());
+ss.close();
+
+}
+catch(Exception e)
+{
+
+System.out.println(e);
+}
+}
+ 
+}
